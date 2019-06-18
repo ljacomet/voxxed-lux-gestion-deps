660f15d25d1f14d8b4e64fe924eccd47cc2ff5ad
Louis Jacomet <ljacomet@gmail.com>
2019-06-18 21:39:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Gestion de dépendances en pratique avec Gradle 5
+
+Présentation pour Voxxed Days Luxembourg 2019
+
+Version en ligne: 
---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("org.gradle.presentation.asciidoctor")
+}
+
+presentation {
+    githubUserName.set("ljacomet")
+    githubRepoName.set("voxxed-lux-gestion-deps")
+}
