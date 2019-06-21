2d4d8e8a12f146109e74578437d3fd98f34c0bec
Louis Jacomet <ljacomet@gmail.com>
2019-06-21 10:41:02 +0200

    Initial producer demo

---
+++ demos/producer/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+group = "my.company"
+version = "1.0.0"
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation("com.google.guava:guava:24.0-jre")
+
+    testImplementation("junit:junit:4.12")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            url = uri("$projectDir/../repo")
+        }
+    }
+}
