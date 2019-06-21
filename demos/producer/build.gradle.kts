7c935d8719f651706c3670365209b4f12823b9b3
Louis Jacomet <ljacomet@gmail.com>
2019-06-21 11:04:17 +0200

    Producer 1.3.0

--- demos/producer/build.gradle.kts
+++ demos/producer/build.gradle.kts
@@ -4,14 +4,14 @@ plugins {
 }
 
 group = "my.company"
-version = "1.1.5"
+version = "1.3.0"
 
 repositories {
     jcenter()
 }
 
 dependencies {
-    implementation("com.google.guava:guava:24.1.1-jre")
+    implementation("com.google.guava:guava:27.0-jre")
 
     testImplementation("junit:junit:4.12")
 }
