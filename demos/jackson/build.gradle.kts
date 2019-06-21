b83fedea037b307f632f4b5b662ae59f4a7da035
Louis Jacomet <ljacomet@gmail.com>
2019-06-21 11:12:24 +0200

    Jackson alignment demo
    Final state

--- demos/jackson/build.gradle.kts
+++ demos/jackson/build.gradle.kts
@@ -8,8 +8,22 @@ repositories {
 }
 
 dependencies {
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.8.9")
-    implementation("io.vertx:vertx-core:3.5.3")
+    implementation(platform("com.fasterxml.jackson:jackson-bom:2.9.5"))
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
+    implementation("io.vertx:vertx-core:3.7.1")
 
     testImplementation("junit:junit:4.12")
+    
+    components.all(JacksonAlignmentRule::class.java)
+}
+
+open class JacksonAlignmentRule : ComponentMetadataRule {
+    override fun execute(ctx: ComponentMetadataContext) {
+        ctx.details.run {
+            if (id.group.startsWith("com.fasterxml.jackson")) {
+                belongsTo("my.jackson:jackson-platform:${id.version}")
+            }
+        }
+    }
+
 }
